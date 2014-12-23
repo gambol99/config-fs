b9d438eedc5f7a781b80fdd67fd47dee173af35e
Rohith <gambol99@gmail.com>
2014-12-23 17:14:17 +0000

    - adding the build out of the directories

--- store/store.go
+++ store/store.go
@@ -60,12 +60,58 @@ func (r *ConfigurationStore) Synchronize() (chan bool,error) {
     return shutdownSignal, nil
 }
 
+func (r *ConfigurationStore) FullPath(path string) string {
+    return *mount_point + path
+}
+
 func (r *ConfigurationStore) BuildFileSystem() error {
+    glog.Infof("Building the file system from k/v stote at: %s", *mount_point )
 
 
 
     return nil
 }
+
+func (r *ConfigurationStore) BuildDirectory(directory string) error {
+    /* step: we get a listing of the files under the directory */
+    listing, err := r.KV.List( directory )
+    if err != nil {
+        glog.Errorf("Failed to get listing from directory: %s, error: %s", directory, err )
+        return err
+    } else {
+        Verbose("BuildDiectory() processing directory: %s", directory )
+        for _, node := range listing {
+            path := r.FullPath( directory )
+            switch {
+            case node.IsFile():
+                /* step: if the file does not exist, create it */
+                if !r.FileFS.Exists(path) {
+                    if err := r.FileFS.Create(path, node.Value); err != nil {
+                        glog.Errorf("Failed to create the file: %s, error: %s", path, err)
+                        continue
+                    }
+                }
+            case node.IsDir():
+                if r.FileFS.Exists(path) == true && r.FileFS.IsDirectory(path) == true {
+                    Verbose("BuildDiectory() item directory: %s already exists, skipping", path)
+                    continue
+                }
+                if !r.FileFS.Exists(path) {
+                    Verbose("BuildDiectory() creating directory item: %s", path)
+                    r.FileFS.Mkdir(path)
+                }
+                /* go recursive and build the contents of that directory */
+                go func() {
+                    if err := r.BuildDirectory(path); err != nil {
+                        glog.Errorf("Failed to build the item directory: %s, error: %s", path, err)
+                    }
+                }()
+            }
+        }
+    }
+    return nil
+}
+
 
 func (r *ConfigurationStore) Close() {
     glog.Infof("Request to shutdown and release the resources" )
