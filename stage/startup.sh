ac9fe5406c90bd74e6a82ca2a0cc006ec5a2bea5
Rohith <gambol99@gmail.com>
2014-12-30 16:39:48 +0000

    [Docker/startup.sh] - adding the startup.sh run file

--- stage/startup.sh
+++ stage/startup.sh
@@ -6,3 +6,13 @@
 #  vim:ts=2:sw=2:et
 #
 
+NAME="Configuration FS"
+MOUNT_POINT=${MOUNT_POINT:-/config}
+CONFIG_URL=${CONFIG_URL:-etcd://localhost:4001}
+DISCOVERY_URL=${DISCOVERY_URL:-consul://localhost:8500}
+VERBOSITY=${VERBOSITY:-3}
+
+mkdir -p ${MOUNT_POINT}
+
+echo "Starting ${NAME}, store: ${CONFIG_URL}, discovery: ${DISCOVERY_URL}"
+bin/config-fs -logtostderr=true -v=${VERBOSITY} -store=${CONFIG_URL}
